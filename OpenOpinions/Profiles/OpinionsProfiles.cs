8f76be6043f175fec870dc00658e0ba9ee3e7a50
Anna Wycinka <anna.wycinka@outlook.com>
2021-08-25 12:55:46 +0200

    add opinions automapper profile

---
+++ OpenOpinions/Profiles/OpinionsProfiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using OpenOpinions.Dtos;
+using OpenOpinions.Models;
+
+namespace OpenOpinions.Profiles
+{
+    public class OpinionsProfiles :Profile
+    {
+        public OpinionsProfiles()
+        {
+            CreateMap<Opinion, ReadOpinionDto>();
+        }
+    }
+}
