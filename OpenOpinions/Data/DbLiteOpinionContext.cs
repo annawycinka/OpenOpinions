e74c92ffba939550743ab2d31d273ff1f398963d
Anna Wycinka <anna.wycinka@outlook.com>
2021-08-30 11:46:13 +0200

    use relative path for lite db

--- OpenOpinions/Data/DbLiteOpinionContext.cs
+++ OpenOpinions/Data/DbLiteOpinionContext.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using LiteDB;
+using Microsoft.Extensions.Hosting;
 using OpenOpinions.Models;
 
 namespace OpenOpinions.Data
 {
     public class DbLiteOpinionContext
     {
-        public readonly LiteDatabase Db = new LiteDatabase(@"C:\Users\annaw\source\repos\OpenOpinions\OpenOpinions\DbLiteOpinion.db");
+        private readonly IHostEnvironment _hostEnvironment;
 
-        
+        public DbLiteOpinionContext(IHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment; 
+        }
+
+        public LiteDatabase Db => new LiteDatabase(@$"{_hostEnvironment.ContentRootPath}\DbLiteOpinion.db");
     }
 }
