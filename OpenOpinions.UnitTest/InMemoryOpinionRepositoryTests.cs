92432e326020ffc398fda130c12f0506d382d2ce
Anna Wycinka <anna.wycinka@outlook.com>
2021-09-16 11:48:13 +0200

    Revert "ddd"
    This reverts commit bc39bb42e4d4aa71e9c4ea906433936449284582.

--- OpenOpinions.UnitTest/InMemoryOpinionRepositoryTests.cs
+++ OpenOpinions.UnitTest/InMemoryOpinionRepositoryTests.cs
@@ -68,7 +68,7 @@ namespace OpenOpinions.UnitTest
         [Fact]
         public async Task WhenGetAllOpinions_ThenAllOpinionsGiven()
         {
-            //Arrangedddddd
+            //Arrange
             var opinion1 = new Opinion
             {
                 Text = "first opinion"
